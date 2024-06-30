d120dad72a69bca2757c24718d04a0fa9f5b6428
Rushikesh Gunjal <rushigunjal2016@gmail.com>
2024-06-30 15:28:46 +0530

    Added media type details

--- Frontend/digital-library/src/app/features/admin/media-type/media-type-detail/media-type-detail.component.ts
+++ Frontend/digital-library/src/app/features/admin/media-type/media-type-detail/media-type-detail.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MediaType } from '../../../../api-client/api-client';
+import { ActivatedRoute, Router } from '@angular/router';
+import { MediaTypeService } from '../services/media-type.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-media-type-detail',
   templateUrl: './media-type-detail.component.html',
-  styleUrl: './media-type-detail.component.scss'
 })
-export class MediaTypeDetailComponent {
+export class MediaTypeDetailComponent implements OnInit {
+  mediaType: MediaType | null = null;
 
+  constructor(
+    private route: ActivatedRoute,
+    private mediaTypeService: MediaTypeService,
+    private router: Router,
+    private _snackBar: MatSnackBar
+  ) {}
+
+  ngOnInit(): void {
+    const idParam = this.route.snapshot.paramMap.get('mediaTypeId');
+    if (idParam) {
+      const mediaTypeId = parseInt(idParam, 10);
+      this.mediaTypeService.getMediaTypeById(mediaTypeId).subscribe({
+        next: (mediaType: MediaType) => {
+          this.mediaType = mediaType;
+        },
+        error: (error: HttpErrorResponse) => {
+          if (error.status === 404) {
+            this._snackBar.open('Media type with given id not found.', 'OK');
+          } else {
+            this._snackBar.open('Something went wrong.', 'OK');
+          }
+          this.goBack();
+        },
+      });
+    }
+  }
+
+  goBack(): void {
+    this.router.navigate(['admin/media-type']);
+  }
 }
